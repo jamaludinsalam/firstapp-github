5d9c27d763c158b23bdb629d3c1e5010ad660ff4
Jems <jamaludinsalam11@gmail.com>
2018-12-15 19:10:57 +0700

    First commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// ====================== Setup ======================
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+//
+// ====================== Routes ======================
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.listen(3000, () =>{
+    console.log("Server Listening on 3000");
+});
