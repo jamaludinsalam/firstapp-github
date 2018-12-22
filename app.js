4d610122aee99cc090dce16bfee0e34cd0f092aa
Jems <jamaludinsalam11@gmail.com>
2018-12-22 07:08:14 +0700

    Modified v2 app.listen

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 
+var port = process.env.PORT || 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -13,7 +14,11 @@ app.get("/", function(req, res){
     res.render("home");
 });
 
-app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("Server was started");
-})
+app.listen(port, function(){
+    console.log("Server was started in port : " + port);
+});
+
+// app.listen(process.env.PORT, process.env.IP, function(){
+//    console.log("Server was started" + );
+// });
 
